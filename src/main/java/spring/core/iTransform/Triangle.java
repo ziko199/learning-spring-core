407f1e96e10a440ca9b3b8fc64e1cbfce970f635
zjouahri <zakaria.jouahri@capgemini.com>
2022-07-22 16:20:09 +0200

    autowire byName

--- src/main/java/spring/core/iTransform/Triangle.java
+++ src/main/java/spring/core/iTransform/Triangle.java
@@ -1,22 +1,38 @@
 package spring.core.iTransform;
 
-import java.util.List;
-
 public class Triangle {
 
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
 
-    public List<Point> getPoints() {
-        return points;
+    public Point getPointA() {
+        return pointA;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     public void draw() {
-        for (Point point: points) {
-            System.out.println("point A: X = " + point.getX() + " Y = " + point.getY());
-        }
+        System.out.println("point A: X = " + getPointA().getX() + " Y = " + getPointA().getY());
+        System.out.println("point B: X = " + getPointB().getX() + " Y = " + getPointB().getY());
+        System.out.println("point C: X = " + getPointC().getX() + " Y = " + getPointC().getY());
     }
 }
