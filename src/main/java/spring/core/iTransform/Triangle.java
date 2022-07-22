8aa9cf633227e978bc69679b487e19aac2e4054d
zjouahri <zakaria.jouahri@capgemini.com>
2022-07-22 15:30:25 +0200

    Injecting Objects

---
+++ src/main/java/spring/core/iTransform/Triangle.java
@@ -0,0 +1,38 @@
+package spring.core.iTransform;
+
+public class Triangle {
+
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+
+    public void draw() {
+        System.out.println("point A: X = " + getPointA().getX() + " Y = " + getPointA().getY());
+        System.out.println("point A: X = " + getPointB().getX() + " Y = " + getPointB().getY());
+        System.out.println("point A: X = " + getPointC().getX() + " Y = " + getPointC().getY());
+    }
+}
