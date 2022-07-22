cbb266a7cb2e869804ac599317282a0b9831a389
zjouahri <zakaria.jouahri@capgemini.com>
2022-07-22 20:56:08 +0200

    Lifecycle Callbacks

--- src/main/java/spring/core/iTransform/DrawingApp.java
+++ src/main/java/spring/core/iTransform/DrawingApp.java
@@ -1,14 +1,14 @@
 package spring.core.iTransform;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        context.registerShutdownHook();
         Triangle triangle = (Triangle) context.getBean("triangle1");
         triangle.draw();
     }
--- src/main/java/spring/core/iTransform/Triangle.java
+++ src/main/java/spring/core/iTransform/Triangle.java
@@ -2,16 +2,19 @@ package spring.core.iTransform;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-public class Triangle implements ApplicationContextAware, BeanNameAware {
+public class Triangle implements ApplicationContextAware, BeanNameAware, InitializingBean, DisposableBean {
 
     private Point pointA;
     private Point pointB;
     private Point pointC;
     private ApplicationContext applicationContext = null;
-
+    private String beanName;
+    static int i = 0;
     public Point getPointA() {
         return pointA;
     }
@@ -42,16 +45,40 @@ public class Triangle implements ApplicationContextAware, BeanNameAware {
         System.out.println("point C: X = " + getPointC().getX() + " Y = " + getPointC().getY());
     }
 
+    public void myInit() {
+        System.out.println("calling myInit method from xml");
+    }
 
+    public void clean() {
+        System.out.println("calling clean method from xml");
+    }
+
+    // ApplicationContextAware
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         System.out.println("Override Method setApplicationContext");
         this.applicationContext = applicationContext;
     }
 
+    // BeanNameAware
     @Override
     public void setBeanName(String beanName) {
         System.out.println("Override Method setBeanName");
         System.out.println("Bean name is: " + beanName);
+        this.beanName = beanName;
+    }
+
+    // InitializingBean
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        i++;
+        System.out.println("InitializingBean init method called for Triangle " + i + " " + this.beanName);
+    }
+
+    // DisposableBean
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean destroy method called for Triangle " + i + " " + this.beanName);
+        i--;
     }
 }
